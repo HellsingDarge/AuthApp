e1753747ae712058fc42cd0a71b68168a9cc3b23
HellsingDarge <20254450+HellsingDarge@users.noreply.github.com>
2020-03-13 18:11:44 +0700

    Справка Этап 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,34 @@
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    args.forEach { println(it) }
-    exitProcess(0)
+    if (args.isEmpty()) {
+        printHelp()
+        exitProcess(1)
+    }
+
+    if (args[0] == "-h" || args[0].isBlank()) {
+        printHelp()
+        exitProcess(1)
+    }
+
+    if (args[0] != "-h") {
+        printHelp()
+        exitProcess(0)
+    }
+}
+
+fun printHelp() {
+    println(
+        """
+        -h - напечатать эту справку
+        -login <str> - логин пользователя, формат - [a-z]{10}
+        -pas <st> - пароль, может быть любым, но как минимум 1 символ
+        -role <str> - роль (READ, WRITE, READ)
+        -res <str> - имя ресурса, полный путь заглавные буквы
+        -ds <YYYY-MM-DD> - начальная дата
+        -de <YYYY-MM-DD> - конечная дата
+        -vol <int> - объём работы, натуральное число
+    """.trimIndent()
+    )
+
 }
