6a12cac804a2345818fb7dd161f452fd486b12db
ABashkirova <alex.bashkirova@gmail.com>
2020-03-18 22:37:35 +0700

    Delete fullname fo options

--- src/ArgHandler.kt
+++ src/ArgHandler.kt
@@ -14,45 +14,38 @@ class ArgHandler(args: Array<String>) {
         val parser = ArgParser("app.jar", true)
         val login by parser.option(
             ArgType.String,
-            "login",
-            "login",
-            "Логин пользователя, строка, строчные буквы. Не более 10 символов"
+            shortName = "login",
+            description = "Логин пользователя, строка, строчные буквы. Не более 10 символов"
         )
         val password by parser.option(
             ArgType.String,
-            "password",
-            "pass",
-            "Пароль"
+            shortName = "pass",
+            description = "Пароль"
         )
         val resource by parser.option(
             ArgType.String,
-            "resource",
-            "res",
-            "Абсолютный путь до запрашиваемого ресурса, формат A.B.C"
+            shortName = "res",
+            description = "Абсолютный путь до запрашиваемого ресурса, формат A.B.C"
         )
         val role by parser.option(
             ArgType.String,
-            "role",
-            "role",
-            "Уровень доступа к ресурсу. Возможные варианты: READ, WRITE, EXECUTE"
+            shortName = "role",
+            description = "Уровень доступа к ресурсу. Возможные варианты: READ, WRITE, EXECUTE"
         )
         val dateStart by parser.option(
             ArgType.String,
-            "date_start",
-            "ds",
-            "Дата начала сессии с ресурсом, формат YYYY-MM-DD "
+            shortName = "ds",
+            description = "Дата начала сессии с ресурсом, формат YYYY-MM-DD "
         )
         val dateEnd by parser.option(
             ArgType.String,
-            "date_end",
-            "de",
-            "Дата окончания сессии с ресурсом, формат YYYY-MM-DD "
+            shortName = "de",
+            description = "Дата окончания сессии с ресурсом, формат YYYY-MM-DD "
         )
         val volume by parser.option(
             ArgType.String,
-            "volume",
-            "vol",
-            "Потребляемый объем, целое число"
+            shortName = "vol",
+            description = "Потребляемый объем, целое число"
         )
         try {
             parser.parse(args)
