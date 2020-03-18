5c531eacedd9d91dfbd183d03efce1303b38fabb
ABashkirova <alex.bashkirova@gmail.com>
2020-03-18 13:41:19 +0700

    Refactor ArgHandler.kt - kotlinx.cli - s6i2

--- src/ArgHandler.kt
+++ src/ArgHandler.kt
@@ -1,12 +1,68 @@
-class ArgHandler(private val args: Array<String>) {
-    enum class Arguments(val value: String) {
-        LOGIN("-login"),
-        PASSWORD("-pass"),
-        RESOURCE("-res"),
-        ROLE("-role"),
-        DATE_START("-ds"),
-        DATE_END("-de"),
-        VOLUME("-vol")
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+
+class ArgHandler(args: Array<String>) {
+    val login: String?
+    val password: String?
+    val resource: String?
+    val role: String?
+    val dateStart: String?
+    val dateEnd: String?
+    val volume: String?
+
+    init {
+        val parser = ArgParser("app.jar", true)
+        val login by parser.option(
+            ArgType.String,
+            "login",
+            "login",
+            "Логин пользователя, строка, строчные буквы. Не более 10 символов"
+        )
+        val password by parser.option(
+            ArgType.String,
+            "password",
+            "pass",
+            "Пароль"
+        )
+        val resource by parser.option(
+            ArgType.String,
+            "resource",
+            "res",
+            "Абсолютный путь до запрашиваемого ресурса, формат A.B.C"
+        )
+        val role by parser.option(
+            ArgType.String,
+            "role",
+            "role",
+            "Уровень доступа к ресурсу. Возможные варианты: READ, WRITE, EXECUTE"
+        )
+        val dateStart by parser.option(
+            ArgType.String,
+            "date_start",
+            "ds",
+            "Дата начала сессии с ресурсом, формат YYYY-MM-DD "
+        )
+        val dateEnd by parser.option(
+            ArgType.String,
+            "date_end",
+            "de",
+            "Дата окончания сессии с ресурсом, формат YYYY-MM-DD "
+        )
+        val volume by parser.option(
+            ArgType.String,
+            "volume",
+            "vol",
+            "Потребляемый объем, целое число"
+        )
+        parser.parse(args)
+
+        this.login = login
+        this.password = password
+        this.resource = resource
+        this.role = role
+        this.dateStart = dateStart
+        this.dateEnd = dateEnd
+        this.volume = volume
     }
 
     fun canTryAuthentication() = args.contains(Arguments.LOGIN.value) && args.contains(Arguments.PASSWORD.value)
