6db7fff99499ac6f8fe4fa1723e3a9ef0e6dc6c1
ABashkirova <alex.bashkirova@gmail.com>
2020-03-20 01:07:10 +0700

    Refactor lazy fields #5 - i3

--- src/ru/kafedrase/authapp/ArgHadler.kt
+++ src/ru/kafedrase/authapp/ArgHadler.kt
@@ -4,63 +4,49 @@ import kotlinx.cli.ArgParser
 import kotlinx.cli.ArgType
 
 class ArgHandler(args: Array<String>) {
-    val login: String?
-    val password: String?
-    val resource: String?
-    val role: String?
-    val dateStart: String?
-    val dateEnd: String?
-    val volume: String?
+    private val parser = ArgParser("app.jar", true)
+
+    var login: String? by parser.option(
+        ArgType.String,
+        shortName = "login",
+        description = "Логин пользователя, строка, строчные буквы. Не более 10 символов"
+    )
+    val password: String? by parser.option(
+        ArgType.String,
+        shortName = "pass",
+        description = "Пароль"
+    )
+    val resource: String? by parser.option(
+        ArgType.String,
+        shortName = "res",
+        description = "Абсолютный путь до запрашиваемого ресурса, формат A.B.C"
+    )
+    val role: String? by parser.option(
+        ArgType.String,
+        shortName = "role",
+        description = "Уровень доступа к ресурсу. Возможные варианты: READ, WRITE, EXECUTE"
+    )
+    val dateStart: String? by parser.option(
+        ArgType.String,
+        shortName = "ds",
+        description = "Дата начала сессии с ресурсом, формат YYYY-MM-DD "
+    )
+    val dateEnd: String? by parser.option(
+        ArgType.String,
+        shortName = "de",
+        description = "Дата окончания сессии с ресурсом, формат YYYY-MM-DD "
+    )
+    val volume: String? by parser.option(
+        ArgType.String,
+        shortName = "vol",
+        description = "Потребляемый объем, целое число"
+    )
 
     init {
-        val parser = ArgParser("app.jar", true)
-        val login by parser.option(
-            ArgType.String,
-            shortName = "login",
-            description = "Логин пользователя, строка, строчные буквы. Не более 10 символов"
-        )
-        val password by parser.option(
-            ArgType.String,
-            shortName = "pass",
-            description = "Пароль"
-        )
-        val resource by parser.option(
-            ArgType.String,
-            shortName = "res",
-            description = "Абсолютный путь до запрашиваемого ресурса, формат A.B.C"
-        )
-        val role by parser.option(
-            ArgType.String,
-            shortName = "role",
-            description = "Уровень доступа к ресурсу. Возможные варианты: READ, WRITE, EXECUTE"
-        )
-        val dateStart by parser.option(
-            ArgType.String,
-            shortName = "ds",
-            description = "Дата начала сессии с ресурсом, формат YYYY-MM-DD "
-        )
-        val dateEnd by parser.option(
-            ArgType.String,
-            shortName = "de",
-            description = "Дата окончания сессии с ресурсом, формат YYYY-MM-DD "
-        )
-        val volume by parser.option(
-            ArgType.String,
-            shortName = "vol",
-            description = "Потребляемый объем, целое число"
-        )
         try {
             parser.parse(args)
         } catch (ex: IllegalStateException) {
         }
-
-        this.login = login
-        this.password = password
-        this.resource = resource
-        this.role = role
-        this.dateStart = dateStart
-        this.dateEnd = dateEnd
-        this.volume = volume
     }
 
     fun canAuthenticate() = !(login.isNullOrBlank() || password.isNullOrBlank())
