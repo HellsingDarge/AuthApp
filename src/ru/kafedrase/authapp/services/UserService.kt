643279d30e55e5a612681562c908b9f996e90512
ABashkirova <alex.bashkirova@gmail.com>
2020-03-23 20:03:05 +0700

    Add try to parsing funcs in UserSession

--- src/ru/kafedrase/authapp/services/UserService.kt
+++ src/ru/kafedrase/authapp/services/UserService.kt
@@ -1,6 +1,7 @@
 package ru.kafedrase.authapp.services
 
 import ru.kafedrase.authapp.Role
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -9,15 +10,23 @@ class UserService {
 
     fun isRoleValid(role: String?) = !role.isNullOrBlank() && Role.getNames().contains(role)
 
-    fun parseDate(date: String): Date {
+    fun parseDate(date: String): Date? = try {
         val formatter = SimpleDateFormat("yyyy-MM-dd")
         formatter.isLenient = false
-        return formatter.parse(date)
+        formatter.parse(date)
+    } catch (ex: ParseException) {
+        null
     }
 
-    fun parseVolume(volume: String) = volume.toInt()
+    fun parseVolume(volume: String): Int? {
+        return try {
+            volume.toInt()
+        } catch (ex: NumberFormatException) {
+            return null
+        }
+    }
 
-    fun areDatesValid(s: Date, e: Date) = s.after(e)
+    fun areDatesValid(start: Date?, end: Date?) = start != null && end != null && end.after(start)
 
-    fun isVolumeValid(volume: Int)= volume > 0
+    fun isVolumeValid(volume: Int?)= volume != null && volume > 0
 }
